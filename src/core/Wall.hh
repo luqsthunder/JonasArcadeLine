b9c47e9ed47b65d675f21762b202475b38729038
Lucas <lucasthund3r@gmail.com>
2016-05-03 21:12:00 -0300

    Adding main to project

---
+++ src/core/Wall.hh
@@ -0,0 +1,8 @@
+#ifndef __JONAS_ARCADE_WALL_HH__
+#define __JONAS_ARCADE_WALL_HH__
+
+class Wall
+{
+};
+
+#endif
---
+++ src/main.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+#include <SDL2/SDL.h>
+
+#include <stdexcept>
+
+int main()
+{
+  if(SDL_Init(SDL_INIT_EVERYTHING) < 0)
+    throw std::runtime_error("cant init sdl");
+
+  SDL_Window* window = SDL_CreateWindow("jonas arcade", SDL_WINDOWPOS_CENTERED,
+                                        SDL_WINDOWPOS_CENTERED, 800, 600,
+                                        SDL_WINDOW_OPENGL | SDL_WINDOW_SHOWN);
+
+  SDL_Renderer render* = SDL_CreateRenderer(window, -1,
+                                            SDL_RENDERER_ACCELERATED |
+                                            SDL_RENDERER_PRESENTVSYNC);
+
+  bool gameRunning = true;
+  SDL_Event event;
+  while(gameRunning)
+  {
+    while(SDL_PollEvent(&event))
+    {
+      if((event.type == SDL_QUIT) ||
+         (event.type == SDL_KEYUP && event.key.keysym.sym == SDLK_ESCAPE))
+        gameRunning = false;
+    }
+    SDL_RenderClear(render);
+
+    SDL_RenderPresent(render);
+  }
+
+  return 0;
+}
